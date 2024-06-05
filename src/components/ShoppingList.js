5f8879168bc98a99ff5c16672cca1e92807643e6
Taylor Ng <taylormng95@gmail.com>
2024-06-05 17:48:45 -0400

    adds filter field functionality

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -5,9 +5,15 @@ import Item from "./Item";
 
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [search, setSearch] = useState('')
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
+  }
+
+  function handleSearch(e){
+    setSearch(e.target.value)
+    console.log(e.target.value)
   }
 
   const itemsToDisplay = items.filter((item) => {
@@ -19,7 +25,7 @@ function ShoppingList({ items }) {
   return (
     <div className="ShoppingList">
       <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <Filter onCategoryChange={handleCategoryChange} handleSearch={handleSearch} search={search} />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
